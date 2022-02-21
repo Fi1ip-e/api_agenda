e30e98db63b475c50c6835aba437cb7e63687cf5
Filipe <filipe-carvalho@outlook.com>
2022-02-21 11:33:02 -0300

    Validacao dos campos telephone

--- readme.md
+++ readme.md
@@ -34,3 +34,20 @@ telephone_2: INTEGER Opicional,
 
 ### Executando a criação da tabela:
 ### npx sequelize-cli db:migrate
+
+## Sobre a aplicação:
+
+A plicação não permite cadastrar algumas informações que ja estão na agenda:
+- O mesmo email;
+- Os mesmos telefones;
+- preencher os dois campos de telephone com o mesmo número.
+
+O campo telephone_2 é opcional, nos testes, no caso de não receber valor, mesmo assim segue a seguinte estrutura:
+http://localhost:8080/contato/cadastrar
+{
+	"first_name": "Jose",
+	"last_name": "Epaminondas",
+	"email": "teste@hotmail.com",
+	"telephone_1": 912345678,
+	"telephone_2": null
+}
--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -19,27 +19,46 @@ routes.post("/cadastrar", async (req, res) => {
 
     //VALIDAÇÕES
     const verificaEmail = await Contato.findOne({ where: { email: req.body.email } });
+    const verificarTelephone_1 = await Contato.findOne({where: {telephone_1: req.body.telephone_1}});
+    const verificarTelephone_2 = await Contato.findOne({where: {telephone_2: req.body.telephone_2}});
+    const verificarTelephone_1_is_2 = await Contato.findOne({where: {telephone_1: req.body.telephone_2}});
+    const verificarTelephone_2_is_1 = await Contato.findOne({where: {telephone_2: req.body.telephone_1}});
 
+    
     if (!req.body.first_name || typeof req.body.first_name == undefined || req.body.first_name == null) {
-        erros.push({ texto: "Nome não pode ser vazio e tem que ser válido" })
+        erros.push({ message: "Nome não pode ser vazio e tem que ser válido" })
     }
 
     if (!req.body.last_name || typeof req.body.last_name == undefined || req.body.last_name == null) {
-        erros.push({ texto: "Sobrenome não pode ser vazio e tem que ser válido" })
+        erros.push({ message: "Sobrenome não pode ser vazio e tem que ser válido" })
     }
 
     if (!req.body.telephone_1 || typeof req.body.telephone_1 == undefined || req.body.telephone_1 == null) {
-        erros.push({ texto: "telephone_1 não pode ser vazio e tem que ser válido" })
+        erros.push({ message: "telephone_1 não pode ser vazio e tem que ser válido" })
     }
 
     if (!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
-        erros.push({ texto: "Email não pode ser vazio e tem que ser válido" })
-    }
-    
-    if (verificaEmail) {
-        erros.push({ texto: "Email ja existe!" });
+        erros.push({ message: "Email não pode ser vazio e tem que ser válido" })
     }
 
+    // VERIFICAR EMAIL
+    if (verificaEmail) {
+        erros.push({ message: "Email ja existe!" });
+    }
+
+    //VERIFICAR TELEFONE
+
+    if (verificarTelephone_1 || verificarTelephone_2) {
+        erros.push({message: "Ja existe esse número registrado em um contato"});
+    }
+    if (verificarTelephone_1_is_2 || verificarTelephone_2_is_1) {
+        erros.push({message: "Ja existe esse número registrado em um contato (teste)"});
+    }
+    if (req.body.telephone_1 == req.body.telephone_2) {
+    erros.push({message: "Erro ao cadastrar numero! Verifique se o número digitado nos dois campos ,não são iguais ou, pode ser que esteja armazenado em algum contato da agenda."});
+    }
+
+    //CAPTURA DE DOS ERROS
     if (erros.length > 0) {
         res.json(erros)
     }
